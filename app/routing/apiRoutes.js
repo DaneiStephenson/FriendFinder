311258f40bc2837dead798ec8e7ed05777b6acdf
Danei Stephenson <daneistephenson@Daneis-MacBook-Air.local>
2017-01-22 23:18:30 -0500

    f

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,33 +2,33 @@ var path = require('path');
 var friends = require('./../data/friends.js');
 
 Array.prototype.min = function() {
-    return Math.min.apply(null, this);
+	return Math.min.apply(null, this);
 };
 
 var friendscores = [];
 for (var i=0; i<friends.length; i++) {
-    friendscores.push(friends[i].score);
+	friendscores.push(friends[i].score);
 }
 
 module.exports = function(app){
     app.post('/api/friends', function(req, res) {
-        var matchscores = [];
-        var matches = [];
-        var userscore = eval(req.body.scores.toString().replace(/,/g,'+'));
-        for (var i=0; i<friendscores.length; i++) {
-            matchscores[i] = Math.abs(userscore-friendscores[i]);
-        }
-        for (var i=0; i<matchscores.length; i++) {
-            if (matchscores.min() == matchscores[i]) {
-                matches.push(friends[i]);
-            }
-        }
-        if (matches.length = 1) {
-            console.log(matches);
-            res.json(matches[0]);
-        }
-        else {
-            res.json(matches[Math.floor(matches.length * Math.random())]);
-        }
-    });
+    	var matchscores = [];
+    	var matches = [];
+    	var userscore = eval(req.body.scores.toString().replace(/,/g,'+'));
+    	for (var i=0; i<friendscores.length; i++) {
+    		matchscores[i] = Math.abs(userscore-friendscores[i]);
+    	}
+    	for (var i=0; i<matchscores.length; i++) {
+    		if (matchscores.min() == matchscores[i]) {
+    			matches.push(friends[i]);
+    		}
+    	}
+    	if (matches.length = 1) {
+    		console.log(matches);
+    		res.json(matches[0]);
+    	}
+    	else {
+    		res.json(matches[Math.floor(matches.length * Math.random())]);
+    	}
+	});
 }
